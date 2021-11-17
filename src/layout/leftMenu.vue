01d93b8f2bf51788e761a74b6d287cb5ba24098b
shargle <332878856@qq.com>
2021-11-17 17:52:43 +0800

    2

--- src/layout/leftMenu.vue
+++ src/layout/leftMenu.vue
@@ -14,10 +14,10 @@
       <el-submenu index="1">
         <template slot="title">
           <i class="el-icon-location"></i>
-          <span slot="title">导航一</span>
+          <span slot="title">导航1</span>
         </template>
         <el-menu-item-group>
-          <span slot="title">分组一</span>
+          <span slot="title">分组1</span>
           <el-menu-item index="1-1">选项1</el-menu-item>
           <el-menu-item index="1-2">选项2</el-menu-item>
         </el-menu-item-group>
